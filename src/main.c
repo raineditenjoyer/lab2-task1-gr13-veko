c9e77aa1838dd41963c7f69cf924845287e1ccb4
Jaroslav Veko <vekoyarik@gmail.com>
2026-03-11 09:34:38 +0000

    func 2

--- src/main.c
+++ src/main.c
@@ -12,3 +12,34 @@ int sumOfDigits(int num) {
 		}
 
 		return sum;
+}
+
+void findNumbersWithMaxDigitSum(int arr[], int n) {
+		if (n <= 0) {
+				printf("Массив пуст или имеет некорректный размер\n");
+				return;
+		}
+
+		int maxSum = 0;
+		for (int i = 0; i < n; i++) {
+				int currentSum = sumOfDigits(arr[i]);
+				if (currentSum > maxSum) {
+						maxSum = currentSum;
+				}
+		}
+	
+		printf("Числа с максимальной суммой цифр (%d):\n", maxSum);
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+				if (sumOfDigits(arr[i]) == maxSum) {
+						printf("%d ", arr[i]);
+						count++;
+				}
+		}
+
+		if (count == 0) {
+				printf("Не найдено чисел с суммой цифр %d", maxSum);
+		}
+		printf("\n");
+}
+	
