bff9886846e5b3ef75da5ae203e5ec5f00c67eb2
Jaroslav Veko <vekoyarik@gmail.com>
2026-03-11 09:33:03 +0000

    func 1

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
-/* main.c */
 #include <stdio.h>
+#include <stdlib.h>
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
-}
+// Функция 1: вычисление суммы цифр числа
+int sumOfDigits(int num) {
+		int sum = 0;
+		num = abs(num);
+
+		while (num > 0) {
+				sum += num % 10;
+				num /= 10;
+		}
+
+		return sum;
